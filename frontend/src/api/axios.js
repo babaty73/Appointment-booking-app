e038ba8c54c5918a106d4ede4af3226e99d94be4
imran <imranidris10999@gmail.com>
2026-06-21 01:04:17 -0700

    deployed on render and removed .env from being displayed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    'https://appointment-backend-epp5.onrender.com/api',
+    'https://appointment-booking-app-b4j5.onrender.com',
 });
 
 export default api;
